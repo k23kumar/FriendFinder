7906d6298d2f07a73b3dda59f79281864be51eb2
k23kumar <k23kumar@gmail.com>
2018-08-18 19:26:19 -0700

    added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,26 +14,60 @@ var friends = [
         4,
         1
       ]
+  },
+
+  {
+    "name":"Michael Scott",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png    ",
+    "scores":[
+        4,
+        3,
+        5,
+        4,
+        2,
+        4,
+        4,
+        5,
+        3,
+        5
+      ]
+  },
+
+  {
+    "name":"Klay Thompson",
+    "photo":"https://www.mercurynews.com/wp-content/uploads/2018/05/bng-warriors-l-0527-60.jpg?w=400",
+    "scores":[
+        1,
+        3,
+        2,
+        5,
+        1,
+        1,
+        4,
+        3,
+        5,
+        1
+      ]
+  },
+
+  {
+    "name":"Cardi B",
+    "photo":"https://s1.r29static.com//bin/entry/5a9/340x408,80/1954881/image.jpg",
+    "scores":[
+        4,
+        4,
+        4,
+        3,
+        3,
+        2,
+        2,
+        2,
+        2,
+        5
+      ]
   }
 
 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
